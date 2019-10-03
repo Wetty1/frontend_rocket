fc128977a65801ea8a2871f1244c18bbefeae1bb
Wesley <wettygamer@gmail.com>
2019-10-03 12:00:32 -0300

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './services/api';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('')
+
+  async function handleSubmit (event) {
+    event.preventDefault();
+    console.log('Hello world')
+    
+    const response = await api.post('/sessions', { email })
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
@@ -13,10 +22,10 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail"/>
-          <button className="btn">Entrar</button>
+          <input type="email" id="email" placeholder="Seu melhor e-mail" onChange={event => setEmail}/>
+          <button className="btn" type="submit">Entrar</button>
         </form>
 
       </div>
